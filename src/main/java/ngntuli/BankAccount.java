bb542f9ba9a24ccf6e27a4df885eb8016f2a9aa2
ngntuli <mr.ng.ntuli@gmail.com>
2022-07-21 16:17:36 +0200

    use this

--- src/main/java/ngntuli/BankAccount.java
+++ src/main/java/ngntuli/BankAccount.java
@@ -1,38 +1,38 @@
 package main.java.ngntuli;
 
 public class BankAccount {
-	private final String accountNumber;
-	private final String accountName;
-	private double balance;
+    private final String accountNumber;
+    private final String accountName;
+    private double balance;
 
-	public BankAccount(String numberIn, String nameIn) {
-		accountNumber = numberIn;
-		accountName = nameIn;
-		balance = 0;
-	}
+    public BankAccount(String accountNumber, String accountName) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        balance = 0;
+    }
 
-	public String getAccountName() {
-		return accountName;
-	}
+    public String getAccountName() {
+        return accountName;
+    }
 
-	public String getAccountNumber() {
-		return accountNumber;
-	}
+    public String getAccountNumber() {
+        return accountNumber;
+    }
 
-	public double getBalance() {
-		return balance;
-	}
+    public double getBalance() {
+        return balance;
+    }
 
-	public void deposit(double amountIn) {
-		balance = balance + amountIn;
-	}
+    public void deposit(double amountIn) {
+        balance = balance + amountIn;
+    }
 
-	public boolean withdraw(double amountIn) {
-		if (amountIn > balance) {
-			return false;
-		} else {
-			balance = balance - amountIn;
-			return true;
-		}
-	}
+    public boolean withdraw(double amount) {
+        if (amount > balance) {
+            return false;
+        } else {
+            balance = balance - amount;
+            return true;
+        }
+    }
 }
